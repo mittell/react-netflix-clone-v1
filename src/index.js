3e95cdb67a418e513f37553adee48a043bc56819
Chris Mittell <mittell@hotmail.co.uk>
2022-10-06 14:40:26 +0900

    feat: added normalize.css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
 import { GlobalStyles } from './global-styles';
+import 'normalize.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
