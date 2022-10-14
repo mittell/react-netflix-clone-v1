ec99ad093ca3210407f56969aa88069ac6889683
Chris Mittell <mittell@hotmail.co.uk>
2022-10-14 15:16:23 +0900

    feat: adding Profile, Loading components

--- src/containers/browse.jsx
+++ src/containers/browse.jsx
@@ -1,5 +1,29 @@
-import React from 'react';
+import React, { useContext, useState, useEffect } from 'react';
+import { SelectProfileContainer } from './profiles';
+import { FirebaseContext } from '../context/firebase';
+import { Loading, Header } from '../components';
 
 export function BrowseContainer({ slides }) {
-	return <></>;
+	const [profile, setProfile] = useState({});
+	const [loading, setLoading] = useState(true);
+	const { fb } = useContext(FirebaseContext);
+	const user = fb.auth().currentUser || {};
+
+	useEffect(() => {
+		setTimeout(() => {
+			setLoading(false);
+		}, 3000);
+	}, [profile.displayName]);
+
+	return profile.displayName ? (
+		<>
+			{loading ? <Loading src={user.photoURL} /> : <Loading.ReleaseBody />}
+
+			<Header src='joker1'>
+				<p>Hello</p>
+			</Header>
+		</>
+	) : (
+		<SelectProfileContainer user={user} setProfile={setProfile} />
+	);
 }
