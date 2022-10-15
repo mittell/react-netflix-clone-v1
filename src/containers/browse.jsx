80bf8589a71320146c41874b8ed4b8204f298d1a
Chris Mittell <mittell@hotmail.co.uk>
2022-10-15 13:16:38 +0900

    feat: added Search Input

--- src/containers/browse.jsx
+++ src/containers/browse.jsx
@@ -6,6 +6,7 @@ import * as ROUTES from '../constants/routes';
 import logo from '../logo.svg';
 
 export function BrowseContainer({ slides }) {
+	const [searchTerm, setSearchTerm] = useState('');
 	const [profile, setProfile] = useState({});
 	const [loading, setLoading] = useState(true);
 	const { fb } = useContext(FirebaseContext);
@@ -29,6 +30,10 @@ export function BrowseContainer({ slides }) {
 						<Header.TextLink>Film</Header.TextLink>
 					</Header.Group>
 					<Header.Group>
+						<Header.Search
+							searchTerm={searchTerm}
+							setSearchTerm={setSearchTerm}
+						/>
 						<Header.Profile>
 							<Header.Picture src={user.photoURL} />
 							<Header.Dropdown>
