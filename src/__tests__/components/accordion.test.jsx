c0541a7b3bc1cb35ce70c4e832e403ecec461df4
Chris Mittell <mittell@hotmail.co.uk>
2022-10-21 09:27:09 +0900

    fix: corrected value comparison

--- src/__tests__/components/accordion.test.jsx
+++ src/__tests__/components/accordion.test.jsx
@@ -43,7 +43,7 @@ describe('<Accordion/>', () => {
 		);
 
 		const whatIsAniflixBodyText =
-			"Aniflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more – on thousands of internet-connected devices.\n\nYou can watch as much as you want, whenever you want, without a single advert – all for one low monthly price. There's always something new to discover, and new TV programmes and films are added every week!";
+			"Aniflix is a streaming service that offers a wide variety of award-winning TV programmes, films, anime, documentaries and more – on thousands of internet-connected devices. You can watch as much as you want, whenever you want, without a single advert – all for one low monthly price. There's always something new to discover, and new TV programmes and films are added every week!";
 
 		expect(queryByText(whatIsAniflixBodyText)).toBeFalsy();
 		fireEvent.click(queryByText('What is Aniflix?'));
